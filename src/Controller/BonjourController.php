c2ab391983f84779ace22240d110657a18a73275
Raphael Da Silva <raphds@yahoo.fr>
2021-11-16 11:09:06 +0100

    Route /bonjour + controleur bonjour

---
+++ src/Controller/BonjourController.php
@@ -0,0 +1,9 @@
+<?php 
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class BonjourController {
+    function bonjour(){
+        return new Response('Bonjour !');
+    }
+}
